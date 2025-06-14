0cf108cd6f5f36726706d9a34cb080283c3081d4
Andrew Weymes <andrew.weymes@sittellalab.com.au>
2025-06-15 02:17:24 +0800

    clean up and add lsps

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,7 +1,7 @@
 return {
 	"saghen/blink.cmp",
-	dependencies = { "rafamadriz/friendly-snippets" },
 	version = "1.*",
+	dependencies = { "L3MON4D3/LuaSnip", version = "v2.*" },
 	opts = {
 		keymap = {
 			["<Up>"] = { "select_prev", "fallback" },
@@ -14,6 +14,7 @@ return {
 		},
 		appearance = { nerd_font_variant = "mono" },
 		completion = { documentation = { auto_show = true } },
+		snippets = { preset = "luasnip" },
 		sources = { default = { "lsp", "path", "snippets", "buffer" } },
 		fuzzy = { implementation = "prefer_rust_with_warning" },
 		opts_extend = { "sources.default" },
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,11 +1,20 @@
 return {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {
-        ensure_installed = { "lua_ls" },
-    },
-    dependencies = {
-        { "mason-org/mason.nvim", opts = {} },
-		{ "j-hui/fidget.nvim", opts = { notification = { window = { winblend = 0 } } } },
-        "neovim/nvim-lspconfig",
-    },
+	"mason-org/mason-lspconfig.nvim",
+	opts = {
+		ensure_installed = {
+			"cssls",
+			"gopls",
+			"html",
+			"htmx",
+			"lua_ls",
+			"pyright",
+			"templ",
+			"ts_ls",
+		},
+	},
+	dependencies = {
+		{ "mason-org/mason.nvim", opts = {} },
+		{ "j-hui/fidget.nvim",    opts = { notification = { window = { winblend = 0 } } } },
+		"neovim/nvim-lspconfig",
+	},
 }
